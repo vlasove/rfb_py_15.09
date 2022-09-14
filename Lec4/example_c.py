2bc6c3f915bae6b6e052aab865c3d0d17f8a0ed9
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-09-14 12:37:49 +0300

    Lec 4 start

---
+++ Lec4/example_c.py
@@ -0,0 +1,26 @@
+# Синтаксис одиночного условного оператора
+# if expression:
+#     body
+
+age = int(input())
+
+if age >= 18:
+    print("Welcome!")
+    print("Second line")
+
+
+print("Program exit")
+
+
+# age = int(input())
+
+# if age >= 18:
+#     print("Your age is:", age)
+#     print("Welcome to our service!")
+#     print("Press here to continue!")
+#     if True:
+#         print("Inner body")
+#         if True:
+#             print("Inner body 2")
+
+# print("Service done!")
