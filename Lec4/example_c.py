5f725018e34a52c49211ac36712f0d751a6aa0e2
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-09-16 10:13:26 +0300

    Lec 4 upd

--- Lec4/example_c.py
+++ Lec4/example_c.py
@@ -2,25 +2,24 @@
 # if expression:
 #     body
 
-age = int(input())
-
-if age >= 18:
-    print("Welcome!")
-    print("Second line")
-
-
-print("Program exit")
-
-
 # age = int(input())
 
 # if age >= 18:
-#     print("Your age is:", age)
-#     print("Welcome to our service!")
-#     print("Press here to continue!")
-#     if True:
-#         print("Inner body")
-#         if True:
-#             print("Inner body 2")
+#     print("Welcome!")   # 4 spacing 
+#     print("Second line")
 
-# print("Service done!")
+# print("Program exit")
+
+
+age = int(input())
+
+if age >= 18:
+    print("Your age is:", age)
+    print("Welcome to our service!")
+    print("Press here to continue!")
+    if True:
+        print("Inner body")
+        if True:
+            print("Inner body 2")
+
+print("Service done!")
---
+++ Lec4/example_m.py
@@ -0,0 +1,10 @@
+# Решение задачи Q
+a_float = float(input())
+b_float = float(input())
+
+result = int(a_float + b_float)
+
+if result % 2 == 0:
+    print("ЧЁТНОЕ")
+else:
+    print("НЕЧЁТНОЕ")
