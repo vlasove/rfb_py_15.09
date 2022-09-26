b34f7962bdf9f856e087e6d6c5a68978905147bc
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-09-26 12:37:50 +0300

    Lec 7 : string comparasion

--- Lec7/example_c.py
+++ Lec7/example_c.py
@@ -5,7 +5,6 @@ while True:
     current_word = input()
     if first_word[-1] == current_word[0]:
         first_word = current_word
-        continue
     else:
         print(current_word)
         break
