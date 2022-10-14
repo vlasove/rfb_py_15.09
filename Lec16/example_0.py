7b630e3e4f4e042af655aaabf0068417b1be65c5
EvgenyVl <evgeny_vlasov@yahoo.com>
2022-10-14 12:30:34 +0300

    Lec16 upload

---
+++ Lec16/example_0.py
@@ -0,0 +1,30 @@
+"""
+Пример анонимной функции
+"""
+
+def add(x_arg:int, y_arg:int):
+    """
+    Арифметическое сложение стеменных аргументов
+    .........
+    """
+    return x_arg ** 3 + y_arg ** (2 + x_arg)
+
+
+def main():
+    """
+    entry point
+    """
+    for i in range(1, 5):
+        print(f"add({i}, {i+1}) is {add(i, i+1)}")
+    print("value in add:", add)
+    print("add type is:", type(add))
+
+    variable_with_lambda = lambda x, y: x ** 3 + y ** (2 + x)
+    print("value lambda:", variable_with_lambda)
+    print("lambda type is:", type(variable_with_lambda))
+    for i in range(1,5):
+        print(f"lambda({i}, {i+1}) is {variable_with_lambda(i, i+1)}")
+
+    
+
+main()
